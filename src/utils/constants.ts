5f5fecf3f06f11eae53f622c4fb6ad7fff91b30e
Mateusz Malec <m.mateusz2000@wp.pl>
2021-09-25 12:47:10 +0200

    feat: create app boilerplate

---
+++ src/utils/constants.ts
@@ -0,0 +1,11 @@
+const size = {
+    tabletBreakpoint: '768px',
+    desktopBreakpoint: '1100px',
+    largeDesktopBreakpoint: '1366px',
+};
+
+export const device = {
+    tabletBreakpoint: `(min-width: ${size.tabletBreakpoint})`,
+    desktopBreakpoint: `(min-width: ${size.desktopBreakpoint})`,
+    largeDesktopBreakpoint: `(min-width: ${size.largeDesktopBreakpoint})`,
+};
