358602bad4d2b7a2a956a894c7e49e7e3fd31a2a
Mateusz Malec <m.mateusz2000@wp.pl>
2021-10-06 22:21:58 +0200

    feat: add game on gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Loader from './components/Loader/Loader';
 import { useAuth } from './hooks/useAuth';
 import { useTicTacToe } from './hooks/useTicTacToe';
